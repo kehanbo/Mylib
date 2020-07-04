ae291d3b10650943b6c8c9c5d7c855022a0355cd
kehanbo <875225214@qq.com>
2020-07-05 00:29:05 +0800

    index

--- MyJs/Mylib.js
+++ MyJs/Mylib.js
@@ -146,7 +146,7 @@ Mylib.prototype = {
         }
     },
     /*************************************************************************************
-     *                                      数据绑定
+     *                                    简单的数据绑定FormateString
      * ***********************************************************************************/
     FormateString: function (str, data) {
         return str.replace(/#\((\w+)\)/g, function (match, key) {
@@ -155,7 +155,7 @@ Mylib.prototype = {
     },
 
     /*************************************************************************************
-     *                                      arttemplate语法
+     *                                    arttemplate语法
      * ***********************************************************************************/
     /****************************【注意要引入arttemplate文件】*****************************/
     //封装成一个函数
@@ -169,46 +169,76 @@ Mylib.prototype = {
     },
 
     /*************************************************************************************
-     *                                     克隆对象
+     *                                     克隆对象/给一个对象扩充功能
      * ***********************************************************************************/
     /*克隆一个json对象*/
-    CopyObject:function (target,source) {
-        for (var i in source){
-            target[i]=source[i]
+    CopyObject: function (target, source) {
+        for (var i in source) {
+            target[i] = source[i]
         }
         return target
     },
     /*可克隆多个json对象*/
     // 功能呢实现CopyObjectMany(target,obj1,obj2,obj3)
-    CopyObjectMany:function () {
-        var key,i = 0,len = arguments.length,target = null,copy;
-        if(len === 0){
+    CopyObjectMany: function () {
+        var key, i = 0, len = arguments.length, target = null, copy;
+        if (len === 0) {
             return;
-        }else if(len === 1){
+        } else if (len === 1) {
             target = this;
-        }else{
+        } else {
             i++;
             target = arguments[0];
         }
-        for(; i < len; i++){
-            for(key in arguments[i]){
+        for (; i < len; i++) {
+            for (key in arguments[i]) {
                 copy = arguments[i][key];
                 target[key] = copy;
             }
         }
         return target;
+    },
+    /*************************************************************************************
+    *                                     随机数
+    * ***********************************************************************************/
+    random: function (begin, end) {
+        return Math.floor(Math.random() * (end - begin)) + begin;
+    },
+    /*************************************************************************************
+    *                                   获取页面传过来的参数
+    *************************************************************************************/
+    simpleQuery: function () {
+        var params = window.location.search;//params:?id,date
+        var arr = params.substring(1).split(",");
+        return arr;
+    },
+    /*************************************************************************************
+    *                                   获取URL查询字符串参数值的通用函数
+    *************************************************************************************/
+    querystring: function () {
+        var str = window.location.search.substring(1);//获取查询字符串，即"id=1&name=location"的部分
+        var arr = str.split("&");//以&符号为界把查询字符串分割成数组
+        var json = {};//定义一个临时对象
+        for (var i = 0; i < arr.length; i++)//遍历数组
+        {
+            var c = arr[i].indexOf("=");//获取每个参数中的等号小标的位置
+            if (c == -1) continue;//如果没有发现测跳到下一次循环继续操作
+            var d = arr[i].substring(0, c);//截取等号前的参数名称，这里分别是id和name
+            var e = arr[i].substring(c + 1);//截取等号后的参数值
+            json[d] = e;//以名/值对的形式存储在对象中
+        }
+        return json;//返回对象
     }
-    
 
 
 
 }
 
 
-    /*************************************************************************************
-     *                                   对象实例化
-     * ***********************************************************************************/
-    var $$ = new Mylib();
+/*************************************************************************************
+ *                                   对象实例化
+ * ***********************************************************************************/
+var $$ = new Mylib();
 
 
 
