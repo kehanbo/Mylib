ad8898637c7e4b305bb3742b0448d9491347275f
kehanbo <875225214@qq.com>
2020-07-09 00:21:20 +0800

    kuangjia

--- MyJs/Mylib.js
+++ MyJs/Mylib.js
@@ -1,116 +1,235 @@
-/*
-************************************************************
-获取元素部分
-************************************************************
-*$id
-*$tag
+/**
+ * 描述：通用框架
+ */
+
+/**根模块**/
+var $$ = function () { };
+$$.prototype = {
+
+    /**将一个对象的属性拷贝给另一个对象*/
+    extend: function (tar, source) {
+        //遍历对象
+        for (var i in source) {
+            tar[i] = source[i];
+        }
+        return tar;
+    }
+
+}
+/**在框架中实例化，这样外面使用的使用就不用实例化了*/
+$$ = new $$();
 
 
 
-************************************************************
-检测数据类型部分
-************************************************************
-*isNumber
-*isBoolean
-*isString
-*isUndefined
-*isObj
-*isNull
-*isArray
-*trim
-*ltrim
-*rtrim
 
+/**基础模块*/
+$$.extend($$,{
+    //给一个对象扩充功能
+    extendMany:function() {
+        var key,i = 0,len = arguments.length,target = null,copy;
+        if(len === 0){
+            return;
+        }else if(len === 1){
+            target = this;
+        }else{
+            i++;
+            target = arguments[0];
+        }
+        for(; i < len; i++){
+            for(key in arguments[i]){
+                copy = arguments[i][key];
+                target[key] = copy;
+            }
+        }
+        return target;
+    }
+})
 
-
-************************************************************
-ajax
-************************************************************
-
-
-
-************************************************************
-arttemplate语法A
-************************************************************
-*formateString
-*
-*
-*/
-
-function Mylib() { }
-Mylib.prototype = {
-    /*************************************************************************************
-     *                                      获取元素部分
-     * ***********************************************************************************/
-    $id: function (id) {
-        return document.getElementById(id)
+/**事件模块*/
+$$.extend($$, {
+    /*绑定事件*/
+    on: function (id, type, fn) {
+        //var dom = document.getElementById(id);
+        var dom = $$.isString(id) ? document.getElementById(id) : id;
+        //如果支持
+        //W3C版本 --火狐 谷歌 等大多数浏览器
+        //如果你想检测对象是否支持某个属性，方法，可以通过这种方式
+        if (dom.addEventListener) {
+            dom.addEventListener(type, fn, false);
+        } else if (dom.attachEvent) {
+            //如果支持 --IE
+            dom.attachEvent('on' + type, fn);
+        }
     },
-    $tag: function (tag) {
+    /*解除事件*/
+    un: function (id, type, fn) {
+        //var dom = document.getElementById(id);
+        var dom = $$.isString(id) ? document.getElementById(id) : id;
+        if (dom.removeEventListener) {
+            dom.removeEventListener(type, fn);
+        } else if (dom.detachEvent) {
+            dom.detachEvent(type, fn);
+        }
+
+    },
+    /*点击*/
+    click: function (id, fn) {
+        this.on(id, 'click', fn);
+    },
+    /*鼠标移上*/
+    mouseover: function (id, fn) {
+        this.on(id, 'mouseover', fn);
+    },
+    /*鼠标离开*/
+    mouseout: function (id, fn) {
+        this.on(id, 'mouseout', fn);
+    },
+    /*悬浮*/
+    hover: function (id, fnOver, fnOut) {
+        if (fnOver) {
+            this.on(id, "mouseover", fnOver);
+        }
+        if (fnOut) {
+            this.on(id, "mouseout", fnOut);
+        }
+    },
+    //事件基础
+    getEvent:function(event){
+        return event?event:window.event;
+    },
+    //获取目标
+    GetTarget :function(event){
+        var e = $$.getEvent(event);
+        return e.target|| e.srcElement;
+    },
+    //组织默认行为
+    preventDefault:function(event){
+        var event = $$.getEvent(event);
+        if(event.preventDefault){
+            event.preventDefault();
+        }else{
+            event.returnValue = false;
+        }
+    },
+    //阻止冒泡
+    stopPropagation:function(event){
+        var event = $$.getEvent(event);
+        if(event.stopPropagation){
+            event.stopPropagation();
+        }else{
+            event.cancelBubble = true;
+        }
+    }
+
+
+
+})
+
+/**选择框架*/
+$$.extend($$,{
+    $id:function (str){
+        return document.getElementById(str)
+    },
+    $tag:function(tag){
         return document.getElementsByTagName(tag)
     },
+    //tab
+    tab:function(id) {
+        //如何获取某个父元素下面的子元素
+        var box = document.getElementById(id);
+        var spans = box.getElementsByTagName('span');
+        var lis = box.getElementsByTagName('li');
 
 
-    /*************************************************************************************
-     *                                      数据类型检测部分A
-     * ***********************************************************************************/
-    IsNumber: function (val) {
+        //两步走
+        //第一步: 先把上半部分实现
+        //群体绑定事件  -- 对所有的span绑定事件
+        //群体绑定事件
+        for(var i=0;i<spans.length;i++) {
+            //相亲法则  -- 给男一号一个代号  --  怎么给 -- 自定义属性
+            spans[i].index=i;
+            spans[i].onmouseover = function() {
+                //排他思想 --  将所有的span置为默认状态  --- 然后再将当前鼠标移上的span置为class -- select
+                for(var i=0;i<spans.length;i++) {
+                    spans[i].className='';
+                    lis[i].className='';
+                }
+                this.className='select';
+                lis[this.index].className='select';
+            }
+        }
+
+    }
+})
+
+/**字符串相关操作模块*/
+$$.extend($$,{
+    //去除左边空格
+    ltrim:function(str){
+        return str.replace(/(^\s*)/g,'');
+    },
+    //去除右边空格
+    rtrim:function(str){
+        return str.replace(/(\s*$)/g,'');
+    },
+    //去除空格
+    trim:function(str){
+        return str.replace(/(^\s*)|(\s*$)/g, '');
+    },
+    //简单的数据绑定formateString
+    formateString:function(str, data){
+        return str.replace(/@\((\w+)\)/g, function(match, key){
+            return typeof data[key] === "undefined" ? '' : data[key]});
+    }
+})
+
+/**日期相关操作模块*/
+$$.extend($$,{})
+
+/**数字相关操作模块*/
+$$.extend($$,{
+    //随机数
+    random: function (begin, end) {
+        return Math.floor(Math.random() * (end - begin)) + begin;
+    }
+})
+
+/**判断数据类型*/
+$$.extend($$,{
+    //数据类型检测
+    isNumber:function (val){
         return typeof val === 'number' && isFinite(val)
     },
-    IsBoolean: function (val) {
-        return typeof val === "boolean";
+    isBoolean:function (val) {
+        return typeof val ==="boolean";
     },
-    IsString: function (val) {
+    isString:function (val) {
         return typeof val === "string";
     },
-    IsUndefined: function (val) {
+    isUndefined:function (val) {
         return typeof val === "undefined";
     },
-    IsObj: function (str) {
-        if (str === null || typeof str === 'undefined') {
+    isObj:function (str){
+        if(str === null || typeof str === 'undefined'){
             return false;
         }
         return typeof str === 'object';
     },
-    IsNull: function (val) {
-        return val === null;
+    isNull:function (val){
+        return  val === null;
     },
-    IsArray: function (arr) {
-        if (arr === null || typeof arr === 'undefined') {
+    isArray:function (arr) {
+        if(arr === null || typeof arr === 'undefined'){
             return false;
         }
         return arr.constructor === Array;
-    },
-    Trim: function (str) {
-        return str.replace(/(^\s*)|(\s*$)/g, '');
-    },
-    Ttrim: function (str) {
-        return str.replace(/(^\s*)/g, '');
-    },
-    Ttrim: function (str) {
-        return str.replace(/(\s*$)/g, '');
-    },
-    //获取数组中最大的值 标准写法     【同理：要获取最小值，那么将max改成min】
-    getMax: function (arr) {
-        var arrlen = arr.length;
-        for (var i = 0, ret = arr[0]; i < arrlen; i++) {
-            ret = Math.max(ret, arr[i])
-        }
-        return ret;
-    },
-    //获取多个数中最大的值  call用法  【同理：要获取最小值，那么将max改成min】
-    getMax2: function () {
-        return Math.max.call(null, 1, 2, 3, 4, 5, 6, 7);
-    },
-    //获取数组中最大的值 apply用法   【同理：要获取最小值，那么将max改成min】
-    getMax3: function (arr) {
-        return Math.max.apply(null, arr);
-    },
+    }
+})
 
-    /*************************************************************************************
-     *                                      ajax封装
-     * ***********************************************************************************/
-     //ajax - 前面我们学习的
-     myAjax:function(URL,fn){
+/**ajax*/
+$$.extend($$,{
+    //ajax - 前面我们学习的
+    myAjax:function(URL,fn){
         var xhr = createXHR();	//返回了一个对象，这个对象IE6兼容。
         xhr.onreadystatechange = function(){
             if(xhr.readyState === 4){
@@ -152,117 +271,10 @@ Mylib.prototype = {
                 throw new Error("No XHR object available.");
             }
         }
-    },
-    /*************************************************************************************
-     *                                    简单的数据绑定FormateString
-     * ***********************************************************************************/
-    FormateString: function (str, data) {
-        return str.replace(/#\((\w+)\)/g, function (match, key) {
-            return typeof data[key] === "undefined" ? '' : data[key]
-        });
-    },
-
-    /*************************************************************************************
-     *                                    arttemplate语法
-     * ***********************************************************************************/
-    /****************************【注意要引入arttemplate文件】*****************************/
-    //封装成一个函数
-    BindTemplate: function (tempalteId, data) {
-        var html = template(tempalteId, data);
-        return html;
-    },
-    ArtTemplate: function (str, data) {
-        var render = template.compile(str);
-        return render(data)
-    },
-
-    /*************************************************************************************
-     *                                     克隆对象/给一个对象扩充功能
-     * ***********************************************************************************/
-    /*克隆一个json对象*/
-    CopyObject: function (target, source) {
-        for (var i in source) {
-            target[i] = source[i]
-        }
-        return target
-    },
-    /*可克隆多个json对象*/
-    // 功能呢实现CopyObjectMany(target,obj1,obj2,obj3)
-    CopyObjectMany: function () {
-        var key, i = 0, len = arguments.length, target = null, copy;
-        if (len === 0) {
-            return;
-        } else if (len === 1) {
-            target = this;
-        } else {
-            i++;
-            target = arguments[0];
-        }
-        for (; i < len; i++) {
-            for (key in arguments[i]) {
-                copy = arguments[i][key];
-                target[key] = copy;
-            }
-        }
-        return target;
-    },
-    /*************************************************************************************
-    *                                     随机数
-    * ***********************************************************************************/
-    random: function (begin, end) {
-        return Math.floor(Math.random() * (end - begin)) + begin;
-    },
-    /*************************************************************************************
-    *                                   获取页面传过来的参数
-    *************************************************************************************/
-    simpleQuery: function () {
-        var params = window.location.search;//params:?id,date
-        var arr = params.substring(1).split(",");
-        return arr;
-    },
-    /*************************************************************************************
-    *                                   获取URL查询字符串参数值的通用函数
-    *************************************************************************************/
-    querystring: function () {
-        var str = window.location.search.substring(1);//获取查询字符串，即"id=1&name=location"的部分
-        var arr = str.split("&");//以&符号为界把查询字符串分割成数组
-        var json = {};//定义一个临时对象
-        for (var i = 0; i < arr.length; i++)//遍历数组
-        {
-            var c = arr[i].indexOf("=");//获取每个参数中的等号小标的位置
-            if (c == -1) continue;//如果没有发现测跳到下一次循环继续操作
-            var d = arr[i].substring(0, c);//截取等号前的参数名称，这里分别是id和name
-            var e = arr[i].substring(c + 1);//截取等号后的参数值
-            json[d] = e;//以名/值对的形式存储在对象中
-        }
-        return json;//返回对象，这里返回的是一个json的对象 
-    },
-    /*************************************************************************************
-    *                                   如何将伪数组转化成真数组
-    *************************************************************************************/
-    //例子：var json={0:'first',2:'second'}  -------->  ["first","second"]
-    callArray: function (json) {
-        return Array.prototype.slice.call(json);
     }
+})
 
+/**数据绑定模块*/
+$$.extend($$,{
 
-
-
-}
-
-
-/*************************************************************************************
- *                                   对象实例化
- * ***********************************************************************************/
-var $$ = new Mylib();
-
-
-
-
-
-
-
-
-
-
-
+})
