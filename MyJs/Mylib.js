dedc725b7cc88ee905fe940d44baffdac474d958
kehanbo <875225214@qq.com>
2020-07-09 00:57:59 +0800

    delegeta

--- MyJs/Mylib.js
+++ MyJs/Mylib.js
@@ -23,20 +23,20 @@ $$ = new $$();
 
 
 /**基础模块*/
-$$.extend($$,{
+$$.extend($$, {
     //给一个对象扩充功能
-    extendMany:function() {
-        var key,i = 0,len = arguments.length,target = null,copy;
-        if(len === 0){
+    extendMany: function () {
+        var key, i = 0, len = arguments.length, target = null, copy;
+        if (len === 0) {
             return;
-        }else if(len === 1){
+        } else if (len === 1) {
             target = this;
-        }else{
+        } else {
             i++;
             target = arguments[0];
         }
-        for(; i < len; i++){
-            for(key in arguments[i]){
+        for (; i < len; i++) {
+            for (key in arguments[i]) {
                 copy = arguments[i][key];
                 target[key] = copy;
             }
@@ -94,31 +94,48 @@ $$.extend($$, {
         }
     },
     //事件基础
-    getEvent:function(event){
-        return event?event:window.event;
+    getEvent: function (event) {
+        return event ? event : window.event;
     },
     //获取目标
-    GetTarget :function(event){
+    GetTarget: function (event) {
         var e = $$.getEvent(event);
-        return e.target|| e.srcElement;
+        return e.target || e.srcElement;
     },
-    //组织默认行为
-    preventDefault:function(event){
+    //阻止默认行为
+    preventDefault: function (event) {
         var event = $$.getEvent(event);
-        if(event.preventDefault){
+        if (event.preventDefault) {
             event.preventDefault();
-        }else{
+        } else {
             event.returnValue = false;
         }
     },
     //阻止冒泡
-    stopPropagation:function(event){
+    stopPropagation: function (event) {
         var event = $$.getEvent(event);
-        if(event.stopPropagation){
+        if (event.stopPropagation) {
             event.stopPropagation();
-        }else{
+        } else {
             event.cancelBubble = true;
         }
+    },
+    //事件委托
+    delegate: function (pid, eventType, selector, fn) {
+        //参数处理
+        var parent = $$.$id(pid);
+        function handle(e) {
+            var target = $$.GetTarget(e);
+            if (target.nodeName.toLowerCase() === selector || target.id === selector || target.className.indexOf(selector) != -1) {
+                // 在事件冒泡的时候，回以此遍历每个子孙后代，如果找到对应的元素，则执行如下函数
+                // 为什么使用call，因为call可以改变this指向
+                // 大家还记得，函数中的this默认指向window，而我们希望指向当前dom元素本身
+                fn.call(target);
+            }
+        }
+        //当我们给父亲元素绑定一个事件，他的执行顺序：先捕获到目标元素，然后事件再冒泡
+        //这里是是给元素对象绑定一个事件
+        parent[eventType] = handle;
     }
 
 
@@ -126,15 +143,15 @@ $$.extend($$, {
 })
 
 /**选择框架*/
-$$.extend($$,{
-    $id:function (str){
+$$.extend($$, {
+    $id: function (str) {
         return document.getElementById(str)
     },
-    $tag:function(tag){
+    $tag: function (tag) {
         return document.getElementsByTagName(tag)
     },
     //tab
-    tab:function(id) {
+    tab: function (id) {
         //如何获取某个父元素下面的子元素
         var box = document.getElementById(id);
         var spans = box.getElementsByTagName('span');
@@ -145,17 +162,17 @@ $$.extend($$,{
         //第一步: 先把上半部分实现
         //群体绑定事件  -- 对所有的span绑定事件
         //群体绑定事件
-        for(var i=0;i<spans.length;i++) {
+        for (var i = 0; i < spans.length; i++) {
             //相亲法则  -- 给男一号一个代号  --  怎么给 -- 自定义属性
-            spans[i].index=i;
-            spans[i].onmouseover = function() {
+            spans[i].index = i;
+            spans[i].onmouseover = function () {
                 //排他思想 --  将所有的span置为默认状态  --- 然后再将当前鼠标移上的span置为class -- select
-                for(var i=0;i<spans.length;i++) {
-                    spans[i].className='';
-                    lis[i].className='';
+                for (var i = 0; i < spans.length; i++) {
+                    spans[i].className = '';
+                    lis[i].className = '';
                 }
-                this.className='select';
-                lis[this.index].className='select';
+                this.className = 'select';
+                lis[this.index].className = 'select';
             }
         }
 
@@ -163,31 +180,32 @@ $$.extend($$,{
 })
 
 /**字符串相关操作模块*/
-$$.extend($$,{
+$$.extend($$, {
     //去除左边空格
-    ltrim:function(str){
-        return str.replace(/(^\s*)/g,'');
+    ltrim: function (str) {
+        return str.replace(/(^\s*)/g, '');
     },
     //去除右边空格
-    rtrim:function(str){
-        return str.replace(/(\s*$)/g,'');
+    rtrim: function (str) {
+        return str.replace(/(\s*$)/g, '');
     },
     //去除空格
-    trim:function(str){
+    trim: function (str) {
         return str.replace(/(^\s*)|(\s*$)/g, '');
     },
     //简单的数据绑定formateString
-    formateString:function(str, data){
-        return str.replace(/@\((\w+)\)/g, function(match, key){
-            return typeof data[key] === "undefined" ? '' : data[key]});
+    formateString: function (str, data) {
+        return str.replace(/@\((\w+)\)/g, function (match, key) {
+            return typeof data[key] === "undefined" ? '' : data[key]
+        });
     }
 })
 
 /**日期相关操作模块*/
-$$.extend($$,{})
+$$.extend($$, {})
 
 /**数字相关操作模块*/
-$$.extend($$,{
+$$.extend($$, {
     //随机数
     random: function (begin, end) {
         return Math.floor(Math.random() * (end - begin)) + begin;
@@ -195,31 +213,31 @@ $$.extend($$,{
 })
 
 /**判断数据类型*/
-$$.extend($$,{
+$$.extend($$, {
     //数据类型检测
-    isNumber:function (val){
+    isNumber: function (val) {
         return typeof val === 'number' && isFinite(val)
     },
-    isBoolean:function (val) {
-        return typeof val ==="boolean";
+    isBoolean: function (val) {
+        return typeof val === "boolean";
     },
-    isString:function (val) {
+    isString: function (val) {
         return typeof val === "string";
     },
-    isUndefined:function (val) {
+    isUndefined: function (val) {
         return typeof val === "undefined";
     },
-    isObj:function (str){
-        if(str === null || typeof str === 'undefined'){
+    isObj: function (str) {
+        if (str === null || typeof str === 'undefined') {
             return false;
         }
         return typeof str === 'object';
     },
-    isNull:function (val){
-        return  val === null;
+    isNull: function (val) {
+        return val === null;
     },
-    isArray:function (arr) {
-        if(arr === null || typeof arr === 'undefined'){
+    isArray: function (arr) {
+        if (arr === null || typeof arr === 'undefined') {
             return false;
         }
         return arr.constructor === Array;
@@ -227,20 +245,20 @@ $$.extend($$,{
 })
 
 /**ajax*/
-$$.extend($$,{
+$$.extend($$, {
     //ajax - 前面我们学习的
-    myAjax:function(URL,fn){
+    myAjax: function (URL, fn) {
         var xhr = createXHR();	//返回了一个对象，这个对象IE6兼容。
-        xhr.onreadystatechange = function(){
-            if(xhr.readyState === 4){
-                if(xhr.status >= 200 && xhr.status < 300 || xhr.status == 304){
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                if (xhr.status >= 200 && xhr.status < 300 || xhr.status == 304) {
                     fn(xhr.responseText);
-                }else{
+                } else {
                     alert("错误的文件！");
                 }
             }
         };
-        xhr.open("get",URL,true);
+        xhr.open("get", URL, true);
         xhr.send();
 
         //闭包形式，因为这个函数只服务于ajax函数，所以放在里面
@@ -251,8 +269,8 @@ $$.extend($$,{
             } else if (typeof ActiveXObject != "undefined") {
                 if (typeof arguments.callee.activeXString != "string") {
                     var versions = ["MSXML2.XMLHttp.6.0", "MSXML2.XMLHttp.3.0",
-                            "MSXML2.XMLHttp"
-                        ],
+                        "MSXML2.XMLHttp"
+                    ],
                         i, len;
 
                     for (i = 0, len = versions.length; i < len; i++) {
@@ -275,6 +293,6 @@ $$.extend($$,{
 })
 
 /**数据绑定模块*/
-$$.extend($$,{
+$$.extend($$, {
 
 })
